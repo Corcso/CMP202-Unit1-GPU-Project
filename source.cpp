cc49b958160eca76f95e8f2def03977acd0f17ee
Corcso <115189987+Corcso@users.noreply.github.com>
2024-04-09 14:19:23 +0100

    Added: SFML Library Setup
    Download here : https://www.sfml-dev.org/download/sfml/2.6.1/
    Accessed 09/04/2024

--- source.cpp
+++ source.cpp
@@ -1,4 +1,24 @@
-int main() {
+#include <SFML/Graphics.hpp>
 
-	return 0;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
